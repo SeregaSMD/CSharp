7d8d45a5e642fe345b3eaf5943bfab2082bb624e
SeregaSMD <tellyoulater>
2022-06-08 18:02:17 +0500

    Task 0-0-v4

--- Sem1/Task_0_Square/Program.cs
+++ Sem1/Task_0_Square/Program.cs
@@ -1,1 +1,7 @@
-﻿// Написать 
+﻿// Написать программу, которая на вход приниамет число и выдает его квадрат
+
+Console.Write("Enter a number: ");
+double number = double.Parse(Console.ReadLine());
+double number_square = number*number;
+Console.Write("The number in square is " + number_square);
+
