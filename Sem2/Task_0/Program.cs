b8b640fcf885a7185d92f61a48b60c52bc4ce8c2
SeregaSMD <maybelater>
2022-06-09 11:44:09 +0500

    FindMax 0v3

--- Sem2/Task_0/Program.cs
+++ Sem2/Task_0/Program.cs
@@ -6,20 +6,11 @@
     return result;
 }
 
-int a1=15;
-int b1=21;
-int c1=391;
-int a2=12;
-int b2=23;
-int c2=33;
-int a3=13;
-int b3=23;
-int c3=33;
-/*
-int max1 =  Max (a1, b1, c1);
-int max2 =  Max (a2, b2, c2);
-int max3 =  Max (a3, b3, c3);
-*/
-int max =  Max (Max (a1, b1, c1), Max (a2, b2, c2), Max (a3, b3, c3));
+int[] array ={11, 21, 31 ,41, 15, 61, 17, 18, 19 };
 
+int max = Max(
+    Max (array[0],array[1],array[2]),
+    Max (array[3],array[4],array[5]),
+    Max (array[6],array[7],array[8])
+);
 Console.WriteLine(max);
