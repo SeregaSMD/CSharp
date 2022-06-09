37c686c480adb8f538d619331726f86b4fe050a4
SeregaSMD <maybelater>
2022-06-09 13:07:47 +0500

    Check even v1

---
+++ Sem2/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number2%number1 == 0)
+{
+    Console.WriteLine("Второе число кратно первому.");
+}
+else
+{
+    Console.WriteLine($"Второе число не кратно первому. Остаток от деления составит {number2%number1}.");
+}
