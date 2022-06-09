51959dafdc2e2a1094cc85eb6441b640ceb7f68d
SeregaSMD <maybelater>
2022-06-09 13:14:15 +0500

    Another check even v1

---
+++ Sem2/Task_5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number%7 == 0 && number%23 == 0)
+{
+    Console.WriteLine($"Введенное число кратно 7 и 23.");
+}
+else
+{
+    Console.WriteLine($"Введенное число не кратно одновременно 7 и 23.");
+}
