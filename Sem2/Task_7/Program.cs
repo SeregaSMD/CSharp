06fbde8bd12319c4b45223a6b4d951d9b7a4a0be
SeregaSMD <maybelater>
2022-06-09 15:04:36 +0500

    Find 2nd numeral in a 3-digit number v1

---
+++ Sem2/Task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your number is {number}.");
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine($"Your number is not a 3 digit number.");
+}
+else
+{
+    Console.WriteLine($"The second numeral of the number is {(number / 10) % 10}.");
+} 
