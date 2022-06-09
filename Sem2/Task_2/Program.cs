c432d2333ae5814eb51d444fbcfd698a82eed1f2
SeregaSMD <maybelater>
2022-06-09 12:27:21 +0500

    FindMaxNumeral v1

---
+++ Sem2/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10, 100);
+
+Console.WriteLine(num);
+
+int c1 = num / 10;
+int c2 = num % 10;
+
+if (c1<c2)
+{
+    Console.WriteLine(c2);
+}
+else
+{
+    Console.WriteLine(c1);
+}
