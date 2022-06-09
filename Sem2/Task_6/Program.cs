f3e7c2cfebfba3658fc3cd6793c50d5aca1f9ca5
SeregaSMD <maybelater>
2022-06-09 14:59:28 +0500

    Find 3rd numeral in a number v1

---
+++ Sem2/Task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your number is {number}.");
+
+if (number < 100)
+{
+    Console.WriteLine($"Your number has less than 3 digits.");
+}
+else
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+
+    Console.WriteLine($"The third numeral of the number is {number % 10}.");
+}
