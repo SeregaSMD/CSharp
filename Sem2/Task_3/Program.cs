5bf5d0daa3bb58c58c9de677665ab8b8e95b8ad9
SeregaSMD <maybelater>
2022-06-09 14:40:41 +0500

    Find 1st and 3st numeral v4

--- Sem2/Task_3/Program.cs
+++ Sem2/Task_3/Program.cs
@@ -1,18 +1,21 @@
 ﻿int num = new Random().Next(100, 1000);
 
 Console.WriteLine($"A random number is: {num}");
+
+/* Преобразование в строку является сложной операцией и ее стоит избегать
 string numstr = num.ToString();
 Console.WriteLine($"The random number in string format is: {numstr}");
 Console.WriteLine($"The first numeral of the number is: {numstr[0]}");
 Console.WriteLine($"The second numeral of the number is: {numstr[1]}");
 Console.WriteLine($"The third numeral of the number is: {numstr[2]}");
+*/
 
-int c1 = num / 100; //первая цифра
-int c2 = (num / 10) % 10; //первая цифра
-int c3 = num % 10; //третья цифра
+int char1 = num / 100; //первая цифра
+int char2 = (num / 10) % 10; //вторая цифра
+int char3 = num % 10; //третья цифра
 
-Console.WriteLine($"The first numeral of the number is: {c1}");
-Console.WriteLine($"The second numeral of the number is: {c2}");
-Console.WriteLine($"The third numeral of the number is: {c3}");
+Console.WriteLine($"The first numeral of the number is: {char1}");
+Console.WriteLine($"The second numeral of the number is: {char2}");
+Console.WriteLine($"The third numeral of the number is: {char3}");
 
-Console.WriteLine($"The random number without 2nd numeral is: {c1}{c3}");
+Console.WriteLine($"The random number without 2nd numeral is: {char1}{char3}");
