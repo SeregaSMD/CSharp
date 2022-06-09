bdd7adf379acb1354e5625439393887c0e6e60ba
SeregaSMD <maybelater>
2022-06-09 12:57:31 +0500

    Find 1st and 3st numeral v2

--- Sem2/Task_3/Program.cs
+++ Sem2/Task_3/Program.cs
@@ -1,11 +1,18 @@
 ﻿int num = new Random().Next(100, 1000);
 
 Console.WriteLine($"Случайное число: {num}");
+string numstr = num.ToString();
+Console.WriteLine($"Случайное число в виде строки: {numstr}");
+Console.WriteLine($"Первая цифра в случайном числе является: {numstr[0]}");
+Console.WriteLine($"Вторая цифра в случайном числе является: {numstr[1]}");
+Console.WriteLine($"Третья цифра в случайном числе является: {numstr[2]}");
 
 int c1 = num / 100; //первая цифра
+int c2 = (num / 10) % 10; //первая цифра
 int c3 = num % 10; //третья цифра
 
 Console.WriteLine($"Первая цифра в случайном числе является: {c1}");
+Console.WriteLine($"Вторая цифра в случайном числе является: {c2}");
 Console.WriteLine($"Третья цифра в случайном числе является: {c3}");
 
 Console.WriteLine($"Случайное число без 2 цифры: {c1}{c3}");
