a2d4b3b4df98156a1848b4ff5909a099cafbe0d4
SeregaSMD <maybelater>
2022-06-09 14:47:07 +0500

    Check even v2

--- Sem2/Task_4/Program.cs
+++ Sem2/Task_4/Program.cs
@@ -1,13 +1,13 @@
-﻿Console.Write("Введите первое число: ");
+﻿Console.Write("Enter first number: ");
 int number1 = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
+Console.Write("Enter second number: ");
 int number2 = int.Parse(Console.ReadLine());
 
 if (number2%number1 == 0)
 {
-    Console.WriteLine("Второе число кратно первому.");
+    Console.WriteLine("The second number is even to the first.");
 }
 else
 {
-    Console.WriteLine($"Второе число не кратно первому. Остаток от деления составит {number2%number1}.");
+    Console.WriteLine($"The second number isn't even to the first. The residual is {number2%number1}.");
 }
