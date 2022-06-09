5dbec5a664e4b2c0d5c7399f0f7088f8d718c59f
SeregaSMD <maybelater>
2022-06-09 14:35:25 +0500

    FindMaxNumeral v4

--- Sem2/Task_2/Program.cs
+++ Sem2/Task_2/Program.cs
@@ -1,15 +1,15 @@
 ﻿int num = new Random().Next(10, 100);
 
-Console.WriteLine($"Случайное число: {num}");
+Console.WriteLine($"Random number is: {num}");
 
 int c1 = num / 10; //первая цифра
 int c2 = num % 10; //вторая цифра
 
 if (c1<c2)
 {
-    Console.WriteLine($"Наибольшей цифрой в случайном числе является: {c2}");
+    Console.WriteLine($"The largest numeral in the number is: {c2}");
 }
 else
 {
-    Console.WriteLine($"Наибольшей цифрой в случайном числе является: {c1}");
+    Console.WriteLine($"The largest numeral in the number is: {c1}");
 }
