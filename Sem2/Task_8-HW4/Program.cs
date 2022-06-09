22d05d64321bb3ae890497da5fea518ee1ebc9f7
SeregaSMD <maybelater>
2022-06-09 16:34:45 +0500

    Find a weekend v1

--- Sem2/Task_8-HW4/Program.cs
+++ Sem2/Task_8-HW4/Program.cs
@@ -1,2 +1,21 @@
-﻿int number = new Random().Next(100, 1000);
-Console.WriteLine($"A random number is {number}.");
+﻿//---------------------------------------------------------------------------------------------------------------------------
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//---------------------------------------------------------------------------------------------------------------------------
+Console.Write($"Enter a weekdaynumber: ");
+int WeekdayNumber = int.Parse(Console.ReadLine());
+Console.WriteLine($"Your weekday number is {WeekdayNumber}.");
+if (WeekdayNumber<=7 && WeekdayNumber>=1)
+{
+    if (WeekdayNumber<=5)
+    {
+        Console.WriteLine($"Your weekday is not a weekend.");
+    }
+    else
+    {
+        Console.WriteLine($"Your weekday is a weekend.");
+    }
+}
+else
+{
+    Console.WriteLine("Your number is not a weekday.");
+}
