28eeefae26a87a0cfaf3bc4e0802172a24063bb2
SeregaSMD <maybelater>
2022-06-23 04:24:58 +0500

    Homework

---
+++ Sem4/Task_Fill_Print/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+    }
+}
+
+int[] mass = new int[8];
+FillArray(mass);
+Console.Write("Созданный массив: ");
+PrintArray(mass);
