072c12f6f76034368fd6602ce5421ddd2d94d818
SeregaSMD <maybelater>
2022-06-23 00:57:09 +0500

    Task_1_Sum

---
+++ Sem4/Task_1_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i=0; i<=A; i++)
+{
+    sum += i;
+}
+Console.Write($"Сумма чисел от 1 до А равна: {sum}");
