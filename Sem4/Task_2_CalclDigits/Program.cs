e5856609f14b46b72579c5db401b1f1b926722a1
SeregaSMD <maybelater>
2022-06-23 19:33:39 +0500

    Task_2_CalcDigitsv3

--- Sem4/Task_2_CalclDigits/Program.cs
+++ Sem4/Task_2_CalclDigits/Program.cs
@@ -1,10 +1,16 @@
 ﻿//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+int digits (int number)
+{
+ int count = 0;
+while (number != 0)
+{
+    number = number /10;
+    count++;
+}   
+return count;
+}
+
 Console.Write("Введите целое число: ");
 int num = int.Parse(Console.ReadLine());
-int count = 0;
-while (num != 0)
-{
-    num = num /10;
-    count++;
-}
-Console.WriteLine("Введенное число имеет " + count + " знаков.");
+
+Console.WriteLine("Введенное число имеет " + digits(num) + " знаков.");
