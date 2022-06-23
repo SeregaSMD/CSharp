84dc1c5256d187d6db6683aea14c4f304aa2dd3c
SeregaSMD <maybelater>
2022-06-23 18:53:55 +0500

    Task_1_Sumv2

--- Sem4/Task_1_Sum/Program.cs
+++ Sem4/Task_1_Sum/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.Write("Введите число А: ");
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+Console.Write("Введите число А: ");
 int A = int.Parse(Console.ReadLine());
+if (A<0)
+{
+    Console.WriteLine("Введите положительное число!");
+    return;
+}
+else
+{
 int sum = 0;
 for (int i=0; i<=A; i++)
 {
     sum += i;
 }
 Console.Write($"Сумма чисел от 0 до {A} равна: {sum}");
+}
