ee4e21553682eab678b3df01bf727989911c0f87
SeregaSMD <maybelater>
2022-07-04 06:57:59 +0500

    Code cleanup

--- Sem7/Task_3_FindEvenIdx/Program.cs
+++ Sem7/Task_3_FindEvenIdx/Program.cs
@@ -11,8 +11,7 @@ void Print(int[,] arr)
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            //Console.Write(i + " " + j + ", ");
+        {            
             Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
