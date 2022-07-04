f888a6c517bc9341646fcb3afdb616cab422944f
SeregaSMD <tellyoulater>
2022-07-04 20:58:50 +0500

    Task_8_Akkerman

---
+++ Sem9/Task_8_Akkerman/Program.cs
@@ -0,0 +1,9 @@
+﻿int Akkerman (int m, int n)
+{
+if (m==0) return n+1;
+if (m>0&&n==0) return Akkerman(m-1,1);
+if (m>0&&n>0) return Akkerman(m-1, Akkerman(m, n-1));
+else return -100500;
+}
+
+Console.WriteLine(Akkerman(3,2));
