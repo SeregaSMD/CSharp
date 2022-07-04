3a8951683e01ae9a6caf9dc12d14421323b79981
SeregaSMD <maybelater>
2022-07-04 07:30:39 +0500

    Homework

--- Sem7/Task_7_ColumnMean/Program.cs
+++ Sem7/Task_7_ColumnMean/Program.cs
@@ -1,26 +1,61 @@
 ﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
-void Print(int[,] arr)
+void PrintArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
-        {            
+        {
             Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-void Fill(int[,] arr)
+void PrintArrayDouble(double[] arr)
 {
-for (int i = 0; i < arr.GetLength(0); i++)
-{
-    for (int j = 0; j < arr.GetLength(1); j++)
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        arr[i, j] = new Random().Next(0, 10);
+        Console.Write(arr[i] + "   ");
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
     }
 }
+
+double FindColumnMean(int[,] arr, int col)
+{
+    double columnSum=0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        columnSum += arr[i, col];
+    }
+    return columnSum / arr.GetLength(0);
 }
-Fill(mass);
-Print(mass);
+
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int col = int.Parse(Console.ReadLine());
+int[,] mass = new int[row, col];
+
+FillArray(mass);
+Console.WriteLine("Сгенерирован массив случайных целых чисел:");
+PrintArray(mass);
+
+double[] columnMean = new double[mass.GetLength(1)];
+for (int j = 0; j < columnMean.Length; j++)
+{
+    columnMean[j] = Math.Round(FindColumnMean(mass, j), 2);
+}
+Console.WriteLine("Среднее арифмитическое по столбцам:");
+PrintArrayDouble(columnMean);
