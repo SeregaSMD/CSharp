559b87e740ee25c3cc40f58dc20634b692aea9f0
SeregaSMD <maybelater>
2022-07-06 17:51:46 +0500

    Task_8_Akkerman

---
+++ Sem9/Task_8_Akkerman/Program.cs
@@ -0,0 +1,13 @@
+﻿int Akkerman (int m, int n)
+{
+if (m==0) return n+1;
+if (m>0 && n==0) return Akkerman(m-1, 1);
+if (m>0 && n>0) return Akkerman (m-1, Akkerman(m, n-1));
+else return -100500;
+}
+
+Console.Write ("Введите число m: ");
+int m=int.Parse(Console.ReadLine());
+Console.Write ("Введите число n: ");
+int n=int.Parse(Console.ReadLine());
+Console.WriteLine ($" функция Аккермана для m={m} и n={n} равна {Akkerman (m,n)}");
