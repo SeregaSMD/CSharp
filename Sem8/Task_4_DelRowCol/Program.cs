04fa9dec6fa3c0c8a8f5c237407f4db6a2f381b3
SeregaSMD <SeregaSMD@example.com>
2022-07-14 17:57:27 +0500

    Task_4_DelRowColv3

--- Sem8/Task_4_DelRowCol/Program.cs
+++ Sem8/Task_4_DelRowCol/Program.cs
@@ -44,38 +44,29 @@ void PrintArray(int[,] arr)
 
 int[,] DelRowCol(int[,] arr, int idx1, int idx2)
 {
-    int correctorI = 0;
-    int correctorJ = 0;
     int[,] newArray = new int[arr.GetLength(0) - 1, arr.GetLength(1) - 1];
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        correctorJ = 0;
         for (int j = 0; j < arr.GetLength(1); j++)
         {
-            if (i != idx1 && j != idx2)
-            {
-                newArray[i - correctorI, j - correctorJ] = arr[i, j];
-            }
-            if (j == idx2) correctorJ = 1;
-            if (i == idx1) correctorI = 1;
+            if (i < idx1 && j < idx2) newArray[i, j] = arr[i, j];
+            if (i < idx1 && j > idx2) newArray[i, j - 1] = arr[i, j];
+            if (i > idx1 && j < idx2) newArray[i - 1, j] = arr[i, j];
+            if (i > idx1 && j > idx2) newArray[i - 1, j - 1] = arr[i, j];
         }
-
     }
     return newArray;
 }
 
-
-
-    Console.Write("Введите кол-во строк: ");
-    int rows = int.Parse(Console.ReadLine());
-    Console.Write("Введите кол-во столбцов: ");
-    int columns = int.Parse(Console.ReadLine());
-    int[,] array = new int[rows, columns];
-    FillArray(array);
-    Console.WriteLine("Исходный массив:");
-    PrintArray(array);
-    (int minIdx1, int minIdx2) = FindMinIdx(array);
-    Console.WriteLine($"Индексы минимального элемента: {minIdx1} и {minIdx2}");
-    int [,] result = DelRowCol(array, minIdx1, minIdx2);
-    PrintArray(result);
-
+Console.Write("Введите кол-во строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] array = new int[rows, columns];
+FillArray(array);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+(int minIdx1, int minIdx2) = FindMinIdx(array);
+Console.WriteLine($"Индексы минимального элемента: {minIdx1} и {minIdx2}");
+int[,] result = DelRowCol(array, minIdx1, minIdx2);
+PrintArray(result);
