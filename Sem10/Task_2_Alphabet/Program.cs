7eb37751a68536f2261c1cafe296001f126a7250
SeregaSMD <SeregaSMD@example.com>
2022-07-17 14:31:53 +0500

    InfoData2to10

---
+++ Sem10/Task_2_Alphabet/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+string[] Words(string[] alf, int size)
+{
+    if (size == 0)
+    {
+        string[] result = new string[1];
+        result[0] = "";
+        return result;
+    }
+    else if (size > 0)
+    {
+        string[] temp = Words(alf, size - 1);
+        string[] result = new string[temp.Length * alf.Length];
+        int index = 0;
+        for (int i = 0; i < temp.Length; i++)
+        {
+            for (int j = 0; j < alf.Length; j++)
+            {
+                result[index] = temp[i] + alf[j];
+                index++;
+            }
+        }
+        return result;
+    }
+    else
+    {
+        string[] result = new string[1];
+        result[0] = "ошибка";
+        return result;
+    }
+
+}
+
+/* Console.Write("Введите допустимые символы алфавита: ");
+string letters = Console.ReadLine(); */
+string[] alf = { "а", "с", "и", "в" };
+Console.Write("Введите длину слова: ");
+int size = int.Parse(Console.ReadLine());
+
+string[] result = Words(alf, size);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.Write(i + " " + result[i] + " ");
+}
---
+++ Sem10/Task_3_InfoData2to10/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 72: Заданы 2 массива: info и data. В массиве info хранятся двоичные представления нескольких чисел (без разделителя).
+// В массиве data хранится информация о количестве бит, которые занимают числа из массива info.
+//Напишите программу, которая составит массив десятичных представлений чисел массива data с учётом информации из массива info.
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+int[] Transform2to10(int[] data2, int [] info2, int [] data10)
+{
+    int corrector = 0;
+    for (int i = 0; i < info2.Length; i++)
+    {
+        for (int j = 0, m = info2[i] - 1; j < info2[i]; j++, m--)
+        {
+            Console.Write($"{data2[j + corrector]}");
+            data10[i] += (int)(data2[j + corrector] * Math.Pow(2, m));
+        }
+        corrector += info2[i];
+        Console.WriteLine();
+    }
+    return data10;
+}
+
+int[] data2 = { 0, 1, 0, 1, 1, 0, 0, 0, 1 };
+int[] info2 = { 3, 2, 3, 1 };
+
+int[] data10 = new int[info2.Length];
+data10=Transform2to10(data2,info2,data10);
+
+PrintArray(data10);
